a293bdbadf2f9f338b88080372e445e969033b35
Supun310 <supuntharaka310@gmail.com>
2024-01-30 20:42:15 +0530

    Admin Page login

---
+++ client/src/pages/AdminPage/updateItems.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UpdateItems = () => {
+    return (
+      <div>
+        {/* Content for Update Items section */}
+        <h2>Update Items Section</h2>
+      </div>
+    );
+  };
+
+  export default UpdateItems;
