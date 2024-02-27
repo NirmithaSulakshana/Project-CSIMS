042717eaf5a6d7691a4265d1dbd3ebaf5c6c3639
Thilina Dinelka <121526622+dinelkathilina@users.noreply.github.com>
2024-02-27 18:17:20 +0530

    add css file

--- client/src/pages/AdminPage/addNewItem.jsx
+++ client/src/pages/AdminPage/addNewItem.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-
+import "../../components/styles/add-new-item.css";
 const AddNewItem = () => {
+
     return (
-      <div>
-        {/* Content for Add New Item section */}
-        <h2>Add New Item Section</h2>
+      <div id="add-new-item-container">
+      
+        <h2>Add New Item</h2>
+        <form id="add-new-item-form">
+          <label>Item Name</label>
+          <input type="text" name="item-name" />
+          <label>Botonical Name</label>
+          <input type="text" name="botonical-name" />
+          <label>Unit Price</label>
+          <input type="number" name="unit-price" />
+          <label>Barcode Number</label>
+          <input type="text" name="barcode-number" />
+          <label>Behaviours</label>
+          <select name="behaviours" class="behaviours">
+            <option value="cool">Cool</option>
+            <option value="not-cool">Not-Cool</option>
+          </select>
+
+          </form>
+        
+
       </div>
+      
     );
   };
 
