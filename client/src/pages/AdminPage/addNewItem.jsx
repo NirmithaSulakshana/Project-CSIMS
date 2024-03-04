10619a7300ead701011709ec57e84c2ec060d703
Thilina Dinelka <121526622+dinelkathilina@users.noreply.github.com>
2024-03-04 12:26:04 +0530

    before the buttons

--- client/src/pages/AdminPage/addNewItem.jsx
+++ client/src/pages/AdminPage/addNewItem.jsx
@@ -8,13 +8,13 @@ const AddNewItem = () => {
         <h2>Add New Item</h2>
         <form id="add-new-item-form">
           <label>Item Name</label>
-          <input type="text" name="item-name" />
+          <input type="text" name="item-name" placeholder="Type Item Name" />
           <label>Botonical Name</label>
-          <input type="text" name="botonical-name" />
+          <input type="text" name="botonical-name" placeholder="Type Botonical Name" />
           <label>Unit Price</label>
-          <input type="number" name="unit-price" />
+          <input type="number" name="unit-price" placeholder="Enter Unit Price" />
           <label>Barcode Number</label>
-          <input type="text" name="barcode-number" />
+          <input type="text" name="barcode-number" placeholder="Enter Barcode Number" />
           <label>Behaviours</label>
           <select name="behaviours" class="behaviours">
             <option value="cool">Cool</option>
