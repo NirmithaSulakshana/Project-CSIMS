62c2353a1ea08ef37611aea544985161ad21e77c
HarshaGihan <harshagihan50@gmail.com>
2024-04-21 10:49:03 +0530

    update order dev

--- client/src/pages/AdminPage/orders.jsx
+++ client/src/pages/AdminPage/orders.jsx
@@ -1,112 +1,41 @@
-import React, { useEffect, useState } from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TablePagination from "@mui/material/TablePagination";
-import axios from "axios";
-import "../../components/styles/adminOrder.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 
 function Order() {
-  const [items, setItems] = useState([]);
-  const [customers, setCustomers] = useState([]);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [userItems, setUserItems] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch items
-        const itemsResponse = await axios.get(
-          "http://localhost:3001/api/items/getItems"
-        );
-        setItems(itemsResponse.data);
-
-        // Fetch customers
-        const customersResponse = await axios.get(
-          "http://localhost:3001/api/users/getApprovedUsers"
-        );
-        setCustomers(customersResponse.data);
-
-        // Fetch user items
-        const userItemsResponse = await axios.get(
-          "http://localhost:3001/api/userItem/getAllUserItems"
-        );
-        setUserItems(userItemsResponse.data.data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
-  const getUserItemQuantity = (itemId, userId) => {
-    const userItem = userItems.find(
-      (item) => item.ItemId === itemId && item.UserId === userId
-    );
-    return userItem ? userItem.quantity : 0;
-  };
-
   return (
-    <div className="containerDivision">
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer sx={{ maxHeight: 600 }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead className="tableHead">
-              <TableRow>
-                <TableCell>Item Name</TableCell>
-                {customers.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.firstName}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {items
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((item) => (
-                  <TableRow key={item.id}>
-                    <TableCell className="tableCellHeads">
-                      {item.itemName}
-                    </TableCell>
-                    {customers.map((customer) => (
-                      <TableCell key={customer.id}>
-                        {getUserItemQuantity(item.id, customer.id)}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={items.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
+    <div>
+      <Box sx={{ width: "100%", maxWidth: 560, bgcolor: "background.paper" }}>
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/AdminPage/CurrentOrder">
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Current Order Details" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/AdminPage/OrderCalender">
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Previous Order Details" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
     </div>
   );
 }
